1b94d89d5aed89186de4f1a74d3d07194f23fb04
DaveCarlson <davicar319@acm.org>
2020-02-12 11:41:13 -0600

    Added in GraphQL support and a simple Hello World GraphQL API.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
 const app = express();
 
 const port = process.env.port || 4000;  
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+const resolvers = {
+    Query: {
+        hello: () => 'Hello World GraphQL'
+    }
+}
+//Setup the Apollo server
+const server = 
+    new ApolloServer({typeDefs, resolvers});
+//Apply the Appolo GraphQL middleware and set the path to /api
+server.applyMiddleware({app, path: '/api'});
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}!!!`));
+app.get('/', 
+    (req, res) => res.send('Hello World!!!'));
+
+app.listen(port, 
+    () => console.log(`Server running at http://localhost:${port}${server.graphqlPath}!!!`));
