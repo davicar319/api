00c80936af74bd6e9a9f9d18648e94769d7db0c1
DaveCarlson <davicar319@acm.org>
2020-02-12 11:18:37 -0600

    Minor Formatting.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 4000;
+const port = process.env.port || 4000;  
 
 app.get('/', (req, res) => res.send('Hello World!!!'));
 
