de10d91e60c848c834a6c078da882247b5c3e8d4
Dave Carlson <dave.carlson@tr.com>
2020-02-13 13:55:27 -0600

    Added ability to get a single note and add a single note.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,45 @@ const app = express();
 
 const port = process.env.port || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Dave Carlson' },
+  { id: '2', content: 'This is another note.', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh look, another note.', author: 'Riley Harrison' }
+];
+
 const typeDefs = gql`
   type Query {
     hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 const resolvers = {
   Query: {
-    hello: () => 'Hello World GraphQL'
+    hello: () => 'Hello World GraphQL',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'David A Carlson'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
   }
 };
 //Setup the Apollo server
