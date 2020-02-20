c2f5afa4450a23a6a9478d6c319c8e548146f474
Dave Carlson <dave.carlson@tr.com>
2020-02-20 13:58:49 -0600

    Moving out the query and mutation into resolvers.

--- src/index.js
+++ src/index.js
@@ -8,32 +8,19 @@ const typeDefs = require('./schema');
 const port = process.env.port || 4000;
 const DB_HOST = process.env.DB_HOST;
 const models = require('./models');
-
-const resolvers = {
-  Query: {
-    notes: () => {
-      return models.Note.find((err, notes) => {
-        return notes;
-      });
-    },
-    note: (parent, args) => {
-      return notes.find(note => note.id === args.id);
-    }
-  },
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: 'Dave Carlson'
-      });
-    }
-  }
-};
+const resolvers = require('./resolvers');
 
 //Connect to the mongoDB
 db.connect(DB_HOST);
 //Setup the Apollo server
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    //Add the database models to the context.
+    return { models };
+  }
+});
 //Apply the Appolo GraphQL middleware and set the path to /api
 server.applyMiddleware({ app, path: '/api' });
 
---
+++ src/resolvers/query.js
@@ -0,0 +1,8 @@
+module.exports = {
+  notes: async (parent, arg, { models }) => {
+    return await models.Note.find();
+  },
+  note: async (parent, args, { models }) => {
+    return await models.Note.findById(args.id);
+  }
+};
