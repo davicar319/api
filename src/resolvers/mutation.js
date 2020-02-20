d8ee6b9c689b70c7496e13beaadf8ef451ed42b2
Dave Carlson <dave.carlson@tr.com>
2020-02-20 14:48:26 -0600

    Adding the ability to update an existing Note.

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -5,6 +5,21 @@ module.exports = {
       author: 'Dave Carlson'
     });
   },
+  updateNote: async (parent, { content, id }, { models }) => {
+    return await models.Note.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          content
+        }
+      },
+      {
+        new: true
+      }
+    );
+  },
   deleteNote: async (parent, { id }, { models }) => {
     try {
       await models.Note.findOneAndRemove({ _id: id });
