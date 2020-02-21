de8c59e5b9992d4b4edf9b489d8ed9d92ce84b09
Dave Carlson <dave.carlson@tr.com>
2020-02-21 15:01:19 -0600

    Resolve the JWT Token and add the user to the context.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const app = express();
@@ -10,15 +11,32 @@ const DB_HOST = process.env.DB_HOST;
 const models = require('./models');
 const resolvers = require('./resolvers');
 
+const getUser = token => {
+  if (token) {
+    try {
+      //return the user information from the token.
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      //If there is a problem with the token, throw an error.
+      throw new Error('Session is invalid.');
+    }
+  }
+};
+
 //Connect to the mongoDB
 db.connect(DB_HOST);
 //Setup the Apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    //Add the database models to the context.
-    return { models };
+  context: ({ req }) => {
+    //Extract the jwt token from the headers.
+    const token = req.headers.authorization;
+    //Retrive the user from the token.
+    const user = getUser(token);
+    //Add the database models and user to the context.
+    console.log(user);
+    return { models, user };
   }
 });
 //Apply the Appolo GraphQL middleware and set the path to /api
